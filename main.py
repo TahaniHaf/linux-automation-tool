c6a1938bc090bbd25c08357d283c850e5cf50914
TahaniHaf <Tahanihaf@icloud.com>
2026-04-19 11:31:24 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,20 @@
-print("Linux Automation Tool is running")
+import os
+import shutil
+
+folder = "test_folder"
+
+if not os.path.exists(folder):
+    os.mkdir(folder)
+
+for i in range(3):
+    file_name = f"{folder}/file{i}.txt"
+    with open(file_name, "w") as f:
+        f.write("This is a test file")
+
+clean_folder = "organized_folder"
+
+if not os.path.exists(clean_folder):
+    os.mkdir(clean_folder)
+
+for file in os.listdir(folder):
+    shutil.move(os.path.join(folder, file), clean_folder)
